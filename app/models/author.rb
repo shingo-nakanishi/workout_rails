05d50b1be72192f21c873023e46b81beca1c6dc0
shingo.nakanishi <shingo7ka24@gmail.com>
2015-10-25 12:33:09 +0900

    :reject_if => lambda { |attributes| attributes[:category_id].blank? } see: http://qiita.com/hmuronaka/items/818c421dc632e3efb7a6

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,4 @@
 class Author < ActiveRecord::Base
   has_many :books, inverse_of: :author
-  accepts_nested_attributes_for :books
+  accepts_nested_attributes_for :books, reject_if: lambda { |attributes| attributes[:title].blank? }
 end
