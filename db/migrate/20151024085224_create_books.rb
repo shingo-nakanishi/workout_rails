174df1048e5b10e9840f5049e7db3bb30b99f840
shingo.nakanishi <shingo7ka24@gmail.com>
2015-10-25 18:05:49 +0900

    fix

--- db/migrate/20151024085224_create_books.rb
+++ db/migrate/20151024085224_create_books.rb
@@ -1,7 +1,7 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
-      t.references :authors, null: false
+      t.references :author, null: false
       t.string :title
 
       t.timestamps null: false
