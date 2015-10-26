acc661c0cef90b6ae6bac7c2c22f7d1a7419f316
shingo.nakanishi <shingo7ka24@gmail.com>
2015-10-26 10:04:00 +0900

    edit post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
+
     ActiveRecord::Base.transaction do
       @post.update!(update_params)
     end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ActiveRecord::Base
   has_many :post_categories, inverse_of: :post
-  accepts_nested_attributes_for :post_categories, reject_if: lambda { |attributes| attributes[:post_category_id].blank? }
+  accepts_nested_attributes_for :post_categories,
+                                allow_destroy: true,
+                                reject_if: :is_reject
+
+  private
+
+  def is_reject(attributes)
+    attributes[:post_category_id].blank?
+  end
 end
